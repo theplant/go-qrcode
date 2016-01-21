401a7fb2f05e5862d375e803384a21986564ea1d
han2015 <1019850453@qq.com>
2016-01-21 11:22:11 +0800

    change some package path

--- qrcode.go
+++ qrcode.go
@@ -45,8 +45,8 @@ import (
 	"log"
 	"os"
 
-	bitset "github.com/skip2/go-qrcode/bitset"
-	reedsolomon "github.com/skip2/go-qrcode/reedsolomon"
+	bitset "github.com/theplant/go-qrcode/bitset"
+	reedsolomon "github.com/theplant/go-qrcode/reedsolomon"
 )
 
 // Encode a QR Code and return a raw PNG image.
